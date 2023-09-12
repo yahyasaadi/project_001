8c7de6ffca09837f3f043d3e18614b19dc25e72b
Suleiman Zeila <suzedu1@gmail.com>
2023-09-12 15:53:32 +0300

    final touches

--- users/views.py
+++ users/views.py
@@ -672,12 +672,13 @@ def list_of_applicants(request):
     owner = OwnerDetails.objects.last
     application = Application.objects.last()
     applied_user_ids = UploadedDocuments.objects.filter( application=application).values_list('user_id', flat=True)
-    applied_users_personal_details = PersonalDetails.objects.filter(user_id__in=applied_user_ids)
+    applied_users_personal_details = PersonalDetails.objects.filter(user_id__in=applied_user_ids, fullname__isnull=False)
+
 
     status_for_all_user = UploadedDocuments.objects.filter(user_id__in=applied_user_ids)
 
 
-    applicants_count = UploadedDocuments.objects.filter(application=application).count()
+    applicants_count = UploadedDocuments.objects.filter(application=application).count() or 0
 
     print(applied_users_personal_details)
     return render(request, 'users/list_of_applicant.html',{
